e5be1dfa1be5f6357dd5853d1b0add6ac6b0fd01
hoangsonhv <70937660+hoangsonhv@users.noreply.github.com>
2021-09-15 00:08:06 +0700

    a

---
+++ Pages/PersonInfomation.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T2008M_UWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PersonInfomation : Page
+    {
+        public PersonInfomation()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string age = txtAge.Text;
+            string email = txtEmail.Text;
+            string address = txtAddress.Text;
+            txtN.Text = name;
+            txtA.Text = age;
+            txtE.Text = email;
+            txtAd.Text = address;
+        }
+    }
+}
